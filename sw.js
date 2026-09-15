5c46206845cbc9b8cca748e1b848b12dafb51b9c
TuNombreDeUsuario <tu-correo@ejemplo.com>
2026-09-14 23:35:11 -0400

    feat(page1): ultra-translucent dock + 3Tree Digital logo v4.3

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@
  * Todos los derechos reservados 2026. CEO Alí Zapata.
  */
 
-const CACHE_NAME = 'diamax-pro-v4.2-hero-translucent';
+const CACHE_NAME = 'diamax-pro-v4.3-ultra-glass';
 const OFFLINE_URL = 'index.html';
 
 const ASSETS_TO_CACHE = [
