eb2d84f769f8e7b0d5bcbf3c941449fccdb4f2bf
TuNombreDeUsuario <tu-correo@ejemplo.com>
2026-09-15 06:20:44 -0400

    feat(ui): replace globe emoji with baseball stadium base SVG on language translator button v5.1

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@
  * Todos los derechos reservados 2026. CEO Alí Zapata.
  */
 
-const CACHE_NAME = 'diamax-pro-v5.0-luxury-step1-form';
+const CACHE_NAME = 'diamax-pro-v5.1-stadium-base-icon';
 const OFFLINE_URL = 'index.html';
 
 const ASSETS_TO_CACHE = [
