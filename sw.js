1b5e927b82e652a052c042ad0232e21a2bb3a0c2
TuNombreDeUsuario <tu-correo@ejemplo.com>
2026-09-15 00:21:08 -0400

    feat(branding): translucent liquid glass capsule for 3Tree logo v4.8

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@
  * Todos los derechos reservados 2026. CEO Alí Zapata.
  */
 
-const CACHE_NAME = 'diamax-pro-v4.7-enlarged-logo';
+const CACHE_NAME = 'diamax-pro-v4.8-translucent-capsule';
 const OFFLINE_URL = 'index.html';
 
 const ASSETS_TO_CACHE = [
