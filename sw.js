8e36462126b67dedea3a8026a05d1bb35f073568
TuNombreDeUsuario <tu-correo@ejemplo.com>
2026-09-15 00:09:30 -0400

    feat(dugout): move exit to page 3, remove membership button, exact inning-by-inning runs tracking v4.6

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@
  * Todos los derechos reservados 2026. CEO Alí Zapata.
  */
 
-const CACHE_NAME = 'diamax-pro-v4.5-pure-cinematic';
+const CACHE_NAME = 'diamax-pro-v4.6-dugout-innings-perfect';
 const OFFLINE_URL = 'index.html';
 
 const ASSETS_TO_CACHE = [
